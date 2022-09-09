36be757a56169c81a64688413d9885e737ae6737
ether0 <etherohm@protonmail.com>
2022-09-09 12:23:45 -0400

    fixed some logic

--- example_scripts/mintSpecificNFT-SpecificCollection.js
+++ example_scripts/mintSpecificNFT-SpecificCollection.js
@@ -6,7 +6,7 @@ const { checkMintStatus } = require("../minting_api/status/checkMintStatus.js");
 // Create a unique contract with a unique NFT. 
 
 let collectionInformation = {
-    collection_name: "Quickstart Demo",
+    collection_name: "Xmint Quickstart Demo",
     collection_description: "Minting a specific NFT id to a specific contract...",
     collection_network: "polygon"
 }
@@ -34,23 +34,28 @@ async function lets_mint(){
     // mint the nft
     console.log(`Minting an NFT to the ${collection_result.id} collection`);
     const mint_response = await mintIdempotency(nft, collection_result.id);
-    const crossmint_id = mint_response.id;
-
-    // check mint status
-    let mint_status = await checkMintStatus(crossmint_id, collection_result.id);
-    while (mint_status.onChain.status == "pending"){
-        // lets check again in 3 seconds
-        console.log("... still working on this ...");
-        await new Promise(r => setTimeout(r, 3000));
-        mint_status = await checkMintStatus(crossmint_id, collection_result.id);
-    }
-
-    // check for success
-    if (mint_status.onChain.status == "success"){
-        console.log(`Your NFT was minted to the ${collection_result.id} collection!\n... txId: ` + mint_status.onChain.txId);
+    if ("error" in mint_response){
+        console.log("Error minting the NFT. Most likely the tokenId already exists.");
     }
     else {
-        console.log("There was an issue minting the NFT.")
+        const crossmint_id = mint_response.id;
+
+        // check mint status
+        let mint_status = await checkMintStatus(crossmint_id, collection_result.id);
+        while (mint_status.onChain.status == "pending"){
+            // lets check again in 3 seconds
+            console.log("... still working on this ...");
+            await new Promise(r => setTimeout(r, 3000));
+            mint_status = await checkMintStatus(crossmint_id, collection_result.id);
+        }
+
+        // check for success
+        if (mint_status.onChain.status == "success"){
+            console.log(`Your NFT was minted to the ${collection_result.id} collection!\n... txId: ` + mint_status.onChain.txId);
+        }
+        else {
+            console.log("There was an issue minting the NFT.")
+        }
     }
 }
 
